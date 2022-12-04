b1a1632e13389620f2e507df46a3906a96c05804
Peter Hofmann <peter.hofmann@formsandlines.eu>
2022-12-04 02:35:37 +0100

    Modified index.clj to recognize my additional notebook.

--- src/advent_of_clerk/index.clj
+++ src/advent_of_clerk/index.clj
@@ -15,21 +15,36 @@
   `src/advent_of_clerk` and filtering out unmodified templates (files
   with less than four lines)."
   []
-  (into []
-        (keep (fn [day]
-                (let [f (fs/file "src" "advent_of_clerk" (format "day_%s.cljc" (cond->> day (<= day 10) (str "0"))))]
-                  (when (and (.exists f)
-                             (< 3 (count (str/split-lines (slurp f)))))
-                    (str f)))))
-        (range 25)))
-
+  (let [root   "src"
+        subdir "advent_of_clerk"]
+    (vec
+     (concat
+      (into []
+            (keep (fn [day]
+                    (let [f (fs/file
+                             root subdir
+                             (format "day_%s.cljc"
+                                     (cond->> day (<= day 10) (str "0"))))]
+                      (when (and (.exists f)
+                                 (< 3 (count (str/split-lines (slurp f)))))
+                        (str f)))))
+            (range 25))
+      ;; additional notebooks
+      [(str (fs/file root subdir "day_02_logic.cljc"))]))))
 
 #_(build-paths)
 
 {:nextjournal.clerk/visibility {:result :show}}
 
 ^::clerk/no-cache
-(clerk/html (into [:ul] (mapv (fn [path]
-                                (when-let [day (second (re-matches #".*day_(\d+).cljc" path))]
-                                  [:li [:a {:href (clerk/doc-url path)} "Day " day]])) (build-paths))))
+(clerk/html
+ (into [:ul]
+       (mapv (fn [path]
+               (let [day      (second (re-matches #".*day_(\d+).*.cljc?" path))
+                     appendix (second (re-matches #".*day_\d+_(.*).cljc?" path))]
+                 (when (some? day)
+                   [:li [:a {:href (clerk/doc-url path)}
+                         "Day " day
+                         (if (some? appendix) (str " / " appendix) "")]])))
+             (build-paths))))
 
